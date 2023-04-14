82e50a4e4bbb8298ffd93003513d92b2d5977f36
Shibin C Reji <scr2404@gmail.com>
2023-04-14 19:47:26 +0530

    Update Speakers.jsx

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,4 +1,4 @@
-import Fade from "react-reveal/Fade";1
+import Fade from "react-reveal/Fade";11
 import { Helmet } from "react-helmet";
 import { Container } from "../components/Container";
 import { SpeakersSection } from "../components/sections/SpeakersSection";
