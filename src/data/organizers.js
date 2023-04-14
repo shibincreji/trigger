3050387fc942303250fd6761e51fbd03b5c2e219
Shibin C Reji <scr2404@gmail.com>
2023-04-14 19:49:56 +0530

    Update organizers.js

--- src/data/organizers.js
+++ src/data/organizers.js
@@ -1,6 +1,6 @@
 export const ORGANIZERS_DATA = [
 	{
-		name: "Sowmya M.N",1111
+		name: "Sowmya M.N",11111
 		event_role: "Publisher",
 		professional_role: "Main Co-ordinator",
 		institution: "CSE, RRCE",
