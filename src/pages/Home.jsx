076f80617c0206f7ea9535b288b82b7c34894c83
Shibin C Reji <scr2404@gmail.com>
2023-06-15 19:12:58 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";111111111111111111
+import { Link } from "react-router-dom";
 import { FiChevronRight } from "react-icons/fi";
 import Fade from "react-reveal/Fade";
 import { Banner } from "../components/sections/Banner"1;
