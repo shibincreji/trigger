7293e35146374255a25f74a5cd10abf6460ccb52
Shibin C Reji <scr2404@gmail.com>
2023-06-23 18:52:41 +0530

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-1111111import PropTypes from "prop-types";11
+11111111import PropTypes from "prop-types";11
 import classNames from "classnames";
 
 export const Container = ({ className, mode,1 children, ...props }) => {
