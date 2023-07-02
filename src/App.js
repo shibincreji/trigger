19157489354c7c5c84cb1c322d5132a9d431b2da
Shibin C Reji <scr2404@gmail.com>
2023-07-02 20:55:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-11111111111import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { Home } from "./pages/Home";
