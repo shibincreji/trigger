d8fb78814e374821db5698391097bf1cbde574e4
Shibin C Reji <scr2404@gmail.com>
2023-07-31 12:44:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-11111111111import React from "react";
+111111111111import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
