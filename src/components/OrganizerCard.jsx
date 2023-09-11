01134152c14fe4b3166af7da1564244b5a7ff6cd
Shibin C Reji <scr2404@gmail.com>
2023-09-11 11:10:05 +0530

    Update OrganizerCard.jsx

--- src/components/OrganizerCard.jsx
+++ src/components/OrganizerCard.jsx
@@ -1,4 +1,4 @@
-111111import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+1111111import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export const OrganizerCard = ({
 	name,
