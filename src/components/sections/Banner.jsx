0017747e70a63efc8656cd0ef1d08d1bda1a26d8
Shibin C Reji <scr2404@gmail.com>
2023-10-05 12:21:39 +0530

    Update Banner.jsx

--- src/components/sections/Banner.jsx
+++ src/components/sections/Banner.jsx
@@ -1,4 +1,4 @@
-import bannerDesktop from '../../assets/images/banner.mp4';
+1import bannerDesktop from '../../assets/images/banner.mp4';
 import bannerMobile from '../../assets/images/banner_mob.mp4';
 
 export const Banner = () => {
