346c6c562bc72508d5186ab3c96a2c98649bd3a7
Shibin C Reji <scr2404@gmail.com>
2023-10-27 11:06:30 +0530

    Update Streams.jsx

--- src/components/sections/Streams.jsx
+++ src/components/sections/Streams.jsx
@@ -1,4 +1,4 @@
-11import { Link } from "react-router-dom";
+111import { Link } from "react-router-dom";
 import { FiChevronRight } from "react-icons/fi";
 import { ROUTES } from "../../routes";
 import { Container } from "../Container";
