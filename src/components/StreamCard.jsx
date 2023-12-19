a6873481e686bc45410d998bb25982695b2236ee
Shibin C Reji <scr2404@gmail.com>
2023-12-19 09:58:24 +0530

    Update StreamCard.jsx

--- src/components/StreamCard.jsx
+++ src/components/StreamCard.jsx
@@ -1,4 +1,4 @@
-1111111import { MdDateRange, MdOndemandVideo } from "react-icons/md";1111
+11111111import { MdDateRange, MdOndemandVideo } from "react-icons/md";1111
 import { BsDot } from "react-icons/bs";
 
 export const StreamCard = ({
