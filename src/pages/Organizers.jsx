8b83633d6e23d586bf2c7f68a1fec135168ca34c
Shibin C Reji <scr2404@gmail.com>
2024-01-31 13:00:16 +0530

    Update Organizers.jsx

--- src/pages/Organizers.jsx
+++ src/pages/Organizers.jsx
@@ -1,4 +1,4 @@
-1import Fade from "react-reveal/Fade";
+11import Fade from "react-reveal/Fade";
 import { Container } from "../components/Container";
 import { Helmet } from "react-helmet";
 import { OrganizerCard } from "../components/OrganizerCard";
