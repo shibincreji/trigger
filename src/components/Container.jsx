fc4174c90841171c70b0a4459afcccde0f9e9d55
shibincreji <scr2404@gmail.com>
2024-03-16 23:14:46 +0530

    fix

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-export const Container = ({ className, mode,1 children, ...props }) => {
+export const Container = ({ className, mode, children, ...props }) => {
 	const modeClass = classNames({
 		"lg:max-w-screen-xl": mode === Container.MODE.NARROW,
 		"lg:max-w-screen-2xl": mode === Container.MODE.MEDIUM,
